0fc06dda911ad7438114ebf14ac974c9c9e95595
Aswinkumar20 <aswinkumar11999@gmail.com>
2022-09-24 15:38:28 +0530

    updated

--- forum/index.js
+++ forum/index.js
@@ -1,4 +1,6 @@
 function showComment(){
     var commentArea = document.getElementById("comment-area");
     commentArea.setAttribute("style", "display:block;");
+    var comment = document.getElementById("commentbutton");
+    comment.setAttribute("style", "display: none");
 }
